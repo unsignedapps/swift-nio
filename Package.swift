69cbdf64ab91954dd55f230ae1d28209a89b2d34
Rob Amos <Robert.Amos@anz.com>
2025-04-10 16:35:19 +1000

    Added another missing one

--- Package.swift
+++ Package.swift
@@ -118,6 +118,7 @@ let package = Package(
 
         // Extras
         .library(name: "_NIODataStructures", targets: ["_NIODataStructures"]),
+        .library(name: "CNIOAtomics", targets: ["CNIOAtomics"]),
         .library(name: "CNIODarwin", targets: [ "CNIODarwin" ]),
         .library(name: "CNIOHTTPParser", targets: [ "CNIOHTTPParser" ]),
         .library(name: "CNIOLinux", targets: [ "CNIOLinux" ]),
